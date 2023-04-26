a81c6016fe017e9f7d3998a1f1432fab746b81a2
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-04-26 10:22:22 +0200

    :ADD Login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Detail from './components/Detail';
+import Login from './components/Login';
 
 import {
   BrowserRouter as Router,
@@ -11,12 +12,14 @@ import {
   Link
 } from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
         <Routes>
+          <Route path="login" element = {<Login/>} />
           <Route path="detail" element={<Detail/>} />
           <Route path="/" element={<Home/>}>
           </Route>
